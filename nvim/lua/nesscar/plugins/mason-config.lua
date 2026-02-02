a04e446f9e9a26ba3e30d5a9723d458dba887a75
c2-07 <114809705+C2-07@users.noreply.github.com>
2026-02-02 18:15:11 +0530

    refactor(mason): update lsp servers to match lsp config
    - Remove pyright from ensure_installed
    - Add lua_ls to ensure_installed (was missing)
    - Organize servers by category (web, systems, python, lua, rust)
    - Add descriptive comments for each server
    - Update to match updated lsp.lua configuration

--- nvim/lua/nesscar/plugins/mason-config.lua
+++ nvim/lua/nesscar/plugins/mason-config.lua
@@ -2,14 +2,24 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	opts = {
 		ensure_installed = {
-			"ts_ls",
-			"eslint",
-			"html",
-			"cssls",
-			"clangd",
-			"rust_analyzer",
-			"pyright",
-			"ruff",
+			-- Web/JavaScript
+			"ts_ls",     -- TypeScript/JavaScript
+			"eslint",    -- JavaScript linting
+			"html",      -- HTML
+			"cssls",     -- CSS
+
+			-- Systems
+			"clangd",         -- C/C++
+			"rust_analyzer",  -- Rust LSP (note: ty provides better type checking)
+
+			-- Python
+			"ruff",      -- Python linting & formatting (all-in-one)
+
+			-- Lua
+			"lua_ls",    -- Lua LSP
+
+			-- Rust type checking (better than rust_analyzer for types)
+			"ty",
 		},
 	},
 }
